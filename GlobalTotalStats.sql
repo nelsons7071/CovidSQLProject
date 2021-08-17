83da93fefbe47f6f25f550b2d9114fbdb367c6f4
nelsons7071 <87828079+nelsons7071@users.noreply.github.com>
2021-08-17 13:19:30 -0700

    Fixed some things up

--- GlobalTotalStats.sql
+++ GlobalTotalStats.sql
@@ -1,5 +1,6 @@
 /* Global Total Stats */
 
-select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as float))/SUM(cast(new_cases as float)) * 100 as worldWideDeathPercentage /*total_deaths, cast(total_deaths as float)/cast(total_cases as float) * 100 as DeathPercentage*/
+select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as float))/SUM(cast(new_cases as float))*100 as DeathPercentage
 from coviddeaths 
+where continent is not null 
 order by 1, 2
